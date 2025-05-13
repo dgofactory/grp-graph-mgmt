92a911514e0d1f33843efeeaf1eaf0c88ce22a8d
dgarcia <scalador@icloud.com>
2025-05-12 21:27:17 -0600

    data

--- src/main/java/dgo/dgmt/DgmtApplication.java
+++ src/main/java/dgo/dgmt/DgmtApplication.java
@@ -3,6 +3,10 @@ package dgo.dgmt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Applications for the best
+ * add a comment
+ */
 @SpringBootApplication
 public class DgmtApplication {
 
